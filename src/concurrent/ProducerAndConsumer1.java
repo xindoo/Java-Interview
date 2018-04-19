de30bcd281515e2e652269bba84c1091011c70f6
xindoo <xindoo@qq.com>
2018-04-19 10:59:19 +0800

    add ProducerAndConsumer1

---
+++ src/concurrent/ProducerAndConsumer1.java
@@ -0,0 +1,84 @@
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * version 1 doesn't use synchronized to improve performance
+ */
+public class ProducerAndConsumer1 {
+    private final int MAX_LEN = 10;
+    private Queue<Integer> queue = new LinkedBlockingQueue<Integer>();
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+    class Producer extends Thread {
+        @Override
+        public void run() {
+            producer();
+        }
+        private void producer() {
+            while(true) {
+                lock.lock();
+                try {
+                    while (queue.size() == MAX_LEN) {
+                        System.out.println("当前队列满");
+                        try {
+                            condition.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    queue.add(1);
+                    condition.signal();
+                    System.out.println("生产者生产一条任务，当前队列长度为" + queue.size());
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+    class Consumer extends Thread {
+        @Override
+        public void run() {
+            consumer();
+        }
+        private void consumer() {
+            while (true) {
+                lock.lock();
+                try {
+                    while (queue.size() == 0) {
+                        System.out.println("当前队列为空");
+                        try {
+                            condition.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    queue.poll();
+                    condition.signal();
+                    System.out.println("消费者消费一条任务，当前队列长度为" + queue.size());
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        ProducerAndConsumer pc = new ProducerAndConsumer();
+        Producer producer = pc.new Producer();
+        Consumer consumer = pc.new Consumer();
+        producer.start();
+        consumer.start();
+    }
+}
